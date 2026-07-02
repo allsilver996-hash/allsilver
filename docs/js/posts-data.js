3a4cd060b3081b99e571d649a92d77e61ef2e636
allsilver996-hash <allsilver996@gmail.com>
2026-07-02 14:10:34 +0900

    Add files via upload

--- docs/js/posts-data.js
+++ docs/js/posts-data.js
@@ -1,3 +1,10 @@
+const SKILLS_POSTS = [
+  { id: 'sql', title: 'SQL (Databricks)', date: '', body: '<p>내용 준비 중</p>' },
+  { id: 'python', title: 'Python', date: '', body: '<p>내용 준비 중</p>' },
+  { id: 'r', title: 'R', date: '', body: '<p>내용 준비 중</p>' },
+  { id: 'tableau', title: 'Tableau', date: '', body: '<p>내용 준비 중</p>' },
+];
+
 const PROJECTS_POSTS = [
   { id: 'a', title: '프로젝트 A', date: '2026.01', body: '<p>내용 준비 중</p>' },
   { id: 'b', title: '프로젝트 B', date: '2026.02', body: '<p>내용 준비 중</p>' },
